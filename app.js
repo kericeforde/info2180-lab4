8268737eec59805968bf1efaa8dc5aa33301ad48
Kerice Forde <fordekerice@gmail.com>
2023-11-11 18:28:12 -0500

    Add new files

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const search=document.querySelector(".btn");
+const request=new XMLHttpRequest();
 
 search.addEventListener("click",function e(){
-    const request=new XMLHttpRequest();
+   
     request.onreadystatechange = function display(){
         if (request.readyState=== XMLHttpRequest.DONE && request.status===200){
-            alert(request.responseText);
+            
+            document.getElementById("result").innerHTML=request.responseText;
         }
     }
-    request.open("GET","superheroes.php");
+    request.open("GET","http://localhost/info2180-lab4/info2180-lab4/superheroes.php",true);
     request.send();
 
 })
