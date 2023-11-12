3ae60ff3ba8d947faae5a0d01b9a295c284a4598
Kerice Forde <fordekerice@gmail.com>
2023-11-12 00:02:22 -0500

    Files for exercise 3

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
+// Initializing variables.
 const search=document.querySelector(".btn");
 const request=new XMLHttpRequest();
+const textform=document.querySelector("form");
 
-search.addEventListener("click",function e(){
-   
-    request.onreadystatechange = function display(){
+//Adding action listener to the form. 
+textform.addEventListener('submit',function(e){
+e.preventDefault();
+
+//Getting value from the textfield.
+const text=document.getElementById("text").value;
+
+//Using AJAX'S XMLHttpRequest to get information from the php file.
+request.onreadystatechange = function display(){
         if (request.readyState=== XMLHttpRequest.DONE && request.status===200){
-            
-            alert(request.responseText);
+            //Adding results to a div.
+            document.getElementById("result").innerHTML=request.responseText;
         }
     }
-    request.open("GET","http://localhost/info2180-lab4/info2180-lab4/superheroes.php",true);
+    request.open("GET",`superheroes.php? query=${text} `,true);
     request.send();
 
-})
+}
+
+
+)
